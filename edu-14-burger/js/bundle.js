0958f266512604b5b14fd3f7f3ffd14dc6291747
AntonVoronezh <pstut@mail.ru>
2018-11-21 19:03:30 +0300

    on check

--- edu-14-burger/js/bundle.js
+++ edu-14-burger/js/bundle.js
@@ -40,6 +40,98 @@ for (let i = 0; i < closeLinkFromNav.length; i += 1) {
     });
 }
 // конец кода меню (скрытое)
+// начало кода слайдер
+const left = document.querySelector(".slider-layout__left");
+const right = document.querySelector(".slider-layout__right");
+const items = document.querySelector(".slider__wrap");
+
+// ширина изображения
+var width = 100;
+// текущий сдвиг влево
+var count = 0;
+// всего слайдов
+const item = document.querySelectorAll(".slider__item");
+
+right.addEventListener("click", function (event) {
+    event.preventDefault();
+    // console.log(item.length, item.length - 2);
+    if (count <= item.length - 2) {
+        count += 1;
+        // console.log(count + '=>');
+        position = width * count;
+        items.style.marginLeft = '-' + position + '%';
+        // console.log(items.style.marginLeft);
+    }
+});
+
+left.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (count !== 0) {
+        count -= 1;
+        // console.log(count + '<=');
+        position = width * count;
+        items.style.marginLeft = '-' + position + '%';
+    }
+});
+// конец кода слайдер
+// Задание 1:
+function filter(input, than) {
+
+    // напишите здесь код, который возвращает новый массив
+    // в котором содержатся те элементы input, которые больше than
+
+    const result = [];
+
+    input.forEach(elem => elem > than ? result.push(elem) : null);
+    return result;
+}
+
+var array = [12, 100, 34, 65, 10];
+var result = filter(array, 60);
+
+console.log(result); // [100, 65];
+
+result = filter(array, 20);
+console.log(result); // [100, 34, 65];
+
+console.log('----------------------------------------');
+// Задание 2:
+
+// Возьмите код предыдущего задания
+// Модифицируйте код функции filter так, чтобы функция выбрасывала исключения при следующих условиях:
+// В input передан пустой массив
+// Хотя бы один из элементов input не является числом
+// Хотя бы один из элементов input является отрицательным числом
+
+
+function filter2(input, than) {
+    const result = [];
+
+    if (input.length === 0) {
+        throw new Error("Пустой массив");
+    } else if (input.some(elem => elem < 0)) {
+        throw new Error("Хотя бы один из элементов input является отрицательным числом");
+    } else if (!input.every(elem => Number.isFinite(elem))) {
+        throw new Error("Хотя бы один из элементов input не является числом");
+    }
+
+    input.forEach(elem => elem > than ? result.push(elem) : null);
+    return result;
+}
+
+// var array = [1, 2, 4, 6, 61];
+var array = [1, 2, 4, 6, 'fff', 61];
+
+try {
+
+    var result = filter2(array, 60);
+    console.log(result); 
+}
+catch (e) {
+    console.log(e.message);
+
+}
+
 // начало кода аккордеон (вертикальный)
 const getTeamSelectors = document.querySelectorAll(".accordion-team__wrap");
 for (let i = 0; i < getTeamSelectors.length; i += 1) {
