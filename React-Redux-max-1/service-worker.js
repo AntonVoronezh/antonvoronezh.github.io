9423e20511807edff9a69221e327e470e6089071
AntonVoronezh <pstut@mail.ru>
2019-06-24 22:34:47 +0300

    ref

--- React-Redux-max-1/service-worker.js
+++ React-Redux-max-1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01b81f22f14672dd18838babadbbbab3.js"
+  "/React-Redux-max-1/precache-manifest.f7553e9441c37a684da1e03838a882d1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-Redux-max-1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
