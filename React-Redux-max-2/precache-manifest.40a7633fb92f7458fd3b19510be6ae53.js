cda103ca40bba53c571b8905c8b8b8a1f9b65189
AntonVoronezh <pstut@mail.ru>
2019-06-26 14:56:05 +0300

    add React-Redux-max-2/

---
+++ React-Redux-max-2/precache-manifest.40a7633fb92f7458fd3b19510be6ae53.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/React-Redux-max-2/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "5a12bc1c76d2c9c6ddd0",
+    "url": "/React-Redux-max-2/static/css/main.e8231da7.chunk.css"
+  },
+  {
+    "revision": "fcb6351e674824443951",
+    "url": "/React-Redux-max-2/static/js/runtime~main.e01bfedc.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/React-Redux-max-2/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "7eab963875f14a5fb1da",
+    "url": "/React-Redux-max-2/static/js/2.92a22952.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/React-Redux-max-2/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/React-Redux-max-2/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/React-Redux-max-2/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/React-Redux-max-2/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/React-Redux-max-2/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/React-Redux-max-2/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "5a12bc1c76d2c9c6ddd0",
+    "url": "/React-Redux-max-2/static/js/main.1518abbb.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/React-Redux-max-2/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/React-Redux-max-2/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/React-Redux-max-2/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/React-Redux-max-2/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/React-Redux-max-2/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/React-Redux-max-2/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/React-Redux-max-2/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/React-Redux-max-2/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "7eab963875f14a5fb1da",
+    "url": "/React-Redux-max-2/static/css/2.47facf54.chunk.css"
+  },
+  {
+    "revision": "cebeac6776b5eab9ef4eb71677e39637",
+    "url": "/React-Redux-max-2/index.html"
+  }
+];
